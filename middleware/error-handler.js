840004fc726d3c5e8d11e3876605f7dc6276b7ae
Francisco Berti da Cruz <fbc991@live.com>
2023-10-10 09:10:11 -0300

    security packages

--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -1,10 +1,30 @@
-const { CustomAPIError } = require('../errors')
-const { StatusCodes } = require('http-status-codes')
-const errorHandlerMiddleware = (err, req, res, next) => {
-  if (err instanceof CustomAPIError) {
-    return res.status(err.statusCode).json({ msg: err.message })
-  }
-  return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ err })
-}
+const { StatusCodes } = require('http-status-codes');
 
-module.exports = errorHandlerMiddleware
+const errorHandlerMiddleware = (err, req, res, next) => {
+  let customError = {
+    statusCode: err.statusCode || StatusCodes.INTERNAL_SERVER_ERROR,
+    msg: err.message || 'something went wrong',
+  };
+
+  if (err.name === 'ValidationError') {
+    customError.msg = Object.values(err.errors)
+      .map((item) => item.message)
+      .join(',');
+    customError.statusCode = StatusCodes.BAD_REQUEST;
+  }
+
+  if (err.code && err.code === 11000) {
+    customError.msg = `duplicate ${Object.keys(err.keyValue)}`;
+    customError.statusCode = StatusCodes.BAD_REQUEST;
+  }
+
+  if (err.name === 'CastError') {
+    customError.msg = `no job found: ${err.value}`;
+    customError.statusCode = StatusCodes.NOT_FOUND;
+  }
+
+  // return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ err });
+  return res.status(customError.statusCode).json({ msg: customError.msg });
+};
+
+module.exports = errorHandlerMiddleware;
