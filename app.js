123f1ebb2b35829c29cca2a3635c0e5492814149
Francisco Berti da Cruz <fbc991@live.com>
2023-10-11 08:02:24 -0300

    nothing

--- app.js
+++ app.js
@@ -21,7 +21,12 @@ const jobsRouter = require('./routes/jobs');
 const notFoundMiddleware = require('./middleware/not-found');
 const errorHandlerMiddleware = require('./middleware/error-handler');
 
-app.set('trust proxy', 1);
+app.use(express.json());
+
+app.use(helmet());
+app.use(cors());
+app.use(xss());
+// app.set('trust proxy', 1);
 app.use(
   rateLimiter({
     windowMs: 15 * 60 * 1000, // 15 minutes
@@ -29,12 +34,7 @@ app.use(
   })
 );
 
-app.use(express.json());
-
-app.use(helmet());
-app.use(cors());
-app.use(xss());
-
+// app.get()
 // routes
 app.use('/api/v1/auth', authRouter);
 //protect route
